e9b74e6ac28bf6545d8ba08be4b5eecb53d31652
Aswad Ransing <ashawatransing@gmail.com>
2026-02-02 23:27:41 +0530

    update

--- categories.js
+++ categories.js
@@ -3,7 +3,7 @@ const categories = [
     id: "soups",
     name: "🥣 Soups",
     file: "data/soups.json",
-    image: "/images/items/soups-cover.webp"
+    image: "images/items/soups-cover.webp"
   },
   {
     id: "indian-starters",
@@ -15,132 +15,132 @@ const categories = [
     id: "chinese-starters",
     name: "🥡 Chinese Starters",
     file: "data/chinese.json",
-    image: "/images/items/chinese-cover.webp"
+    image: "images/items/chinese-cover.webp"
   },
   {
     id: "chinese-gravy",
     name: "🍲 Chinese Gravy",
     file: "data/chinese-gravy.json",
-    image: "/images/items/chinese-gravy-cover.webp"
+    image: "images/items/chinese-gravy-cover.webp"
   },
   {
     id: "main-course",
     name: "🍛 Main Course (Veg)",
     file: "data/main-course.json",
-    image: "/images/items/main-course-cover.webp"
+    image: "images/items/main-course-cover.webp"
   },
   {
     id: "paneer",
     name: "🧀 Paneer Special",
     file: "data/paneer.json",
-    image: "/images/items/paneer-cover.webp"
+    image: "images/items/paneer-cover.webp"
   },
   {
     id: "dal",
     name: "🥣 Dal",
     file: "data/dal.json",
-     image: "/images/items/dal-cover.webp" 
+     image: "images/items/dal-cover.webp" 
   },
   {
     id: "moghlai",
     name: "👑 Moghlai Veg",
     file: "data/moghlai.json",
-     image: "/images/items/moghlai-cover.webp"
+     image: "images/items/moghlai-cover.webp"
   },
   {
     id: "roti-breads",
     name: "🫓 Roti & Breads",
     file: "data/roti-breads.json",
-     image: "/images/items/roti-breads-cover.webp"
+     image: "images/items/roti-breads-cover.webp"
   },
   {
     id: "snacks",
     name: "🍟 Snacks",
     file: "data/snacks.json",
-     image: "/images/items/snacks-cover.webp"
+     image: "images/items/snacks-cover.webp"
   },
   {
     id: "sandwiches",
     name: "🥪 Sandwiches",
     file: "data/sandwiches.json",
-     image: "/images/items/sandwiches-cover.webp"
+     image: "images/items/sandwiches-cover.webp"
   },
   {
     id: "continental-starters",
     name: "🌮 Continental Starters",
     file: "data/continental-starters.json",
-     image: "/images/items/continental-starters-cover.webp"
+     image: "images/items/continental-starters-cover.webp"
   },
   {
     id: "continental-mains",
     name: "🍝 Continental Mains (Pasta)",
     file: "data/continental-mains.json",
-    image: "/images/items/continental-mains-cover.webp"
+    image: "images/items/continental-mains-cover.webp"
   },
   {
     id: "pizza",
     name: "🍕 Pizza",
     file: "data/pizza.json",
-     image: "/images/items/pizza-cover.webp"
+     image: "images/items/pizza-cover.webp"
   },
   {
     id: "sizzlers",
     name: "🔥 Sizzlers",
     file: "data/sizzlers.json",
-     image: "/images/items/sizzlers-cover.webp"
+     image: "images/items/sizzlers-cover.webp"
   },
   {
     id: "papad-raita",
     name: "🥗 Papad / Raita / Salad",
     file: "data/papad-raita.json",
-     image: "/images/items/papad-raita-cover.webp"
+     image: "images/items/papad-raita-cover.webp"
   },
   {
     id: "pav-bhaji",
     name: "🍞 Pav Bhaji",
     file: "data/pav-bhaji.json",
-     image: "/images/items/pav-bhaji-cover.webp"
+     image: "images/items/pav-bhaji-cover.webp"
   },
   {
     id: "desserts",
     name: "🍨 Fruit Salad & Desserts",
     file: "data/desserts.json",
-    image: "/images/items/chinese-cover.webp"
+    image: "images/items/chinese-cover.webp"
   },
   {
     id: "mastani",
     name: "🥤 Mastani",
     file: "data/mastani.json",
-    image: "/images/items/mastani-cover.webp"
+    image: "images/items/mastani-cover.webp"
   },
   {
     id: "ice-cream",
     name: "🍦 Ice Cream",
     file: "data/ice-cream.json",
-     image: "/images/items/ice-cream-cover.webp"
+     image: "images/items/ice-cream-cover.webp"
   },
   {
     id: "falooda",
     name: "🍹 Falooda & Milkshakes",
     file: "data/falooda.json",
-     image: "/images/items/falooda-cover.webp"
+     image: "images/items/falooda-cover.webp"
   },
   {
     id: "mocktails",
     name: "🍸 Mocktails",
     file: "data/mocktails.json",
-    image: "/images/items/mocktails-cover.webp"
+    image: "images/items/mocktails-cover.webp"
   },
   {
     id: "hot-beverages",
     name: "☕ Hot Beverages",
     file: "data/hot-beverages.json",
-     image: "/images/items/hot-beverages-cover.webp"
+     image: "images/items/hot-beverages-cover.webp"
   },
   {
     id: "cold-beverages",
     name: "🧃 Cold Beverages",
     file: "data/cold-beverages.json",
-     image: "/images/items/cold-beverages-cover.webp"
+     image: "images/items/cold-beverages-cover.webp"
   }
 ];
